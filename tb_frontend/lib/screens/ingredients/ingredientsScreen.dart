68bed8764ecc47051bebbda743f5927aa66875af
yanik23 <ylange@windowslive.com>
2023-07-08 23:37:26 +0200

    [wip] search bar seems to work
    Signed-off-by: yanik23 <ylange@windowslive.com>

--- tb_frontend/lib/screens/ingredients/ingredientsScreen.dart
+++ tb_frontend/lib/screens/ingredients/ingredientsScreen.dart
@@ -19,6 +19,11 @@ class IngredientsScreen extends StatefulWidget {
 class _IngredientsScreenState extends State<IngredientsScreen> {
   late Future<List<Ingredient>> ingredients;
   List<Ingredient> localIngredients = [];
+  List<Ingredient> items = [];
+
+  bool _showSearchBar = false;
+
+  TextEditingController editingController = TextEditingController();
 
   @override
   void initState() {
@@ -26,6 +31,7 @@ class _IngredientsScreenState extends State<IngredientsScreen> {
     ingredients = fetchIngredients();
     ingredients.then((value) => {
       localIngredients.addAll(value),
+      items.addAll(value)
       /*localIngredients.forEach((element) {
         element.status = "ok";
         element.remoteId = 23;
@@ -83,49 +89,120 @@ class _IngredientsScreenState extends State<IngredientsScreen> {
     });
   }
 
+  void _filterSearchResults(String query) {
+    List<Ingredient> dummySearchList = [];
+    dummySearchList.addAll(localIngredients);
+    if(query.isNotEmpty) {
+      List<Ingredient> dummyListData = [];
+      dummySearchList.forEach((item) {
+        if(item.name.toLowerCase().contains(query.toLowerCase())) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        localIngredients.clear();
+        localIngredients.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        localIngredients.clear();
+        localIngredients.addAll(dummySearchList);
+      });
+    }
+  }
+
+  void filterSearchResults(String query) {
+    setState(() {
+      items = localIngredients
+          .where((item) => item.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
+    Widget searchBar = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        onChanged: (value) {
+          filterSearchResults(value);
+        },
+        controller: editingController,
+        decoration: InputDecoration(
+          labelText: "Search",
+          hintText: "Search",
+          prefixIcon: Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+          ),
+        ),
+      ),
+    );
+
     Widget content = FutureBuilder<List<Ingredient>>(
       future: ingredients,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return RefreshIndicator(
             onRefresh: _refreshIngredients,//_refreshIngredients
-            child: ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                return Dismissible(
-                  key: UniqueKey(),
-                    onDismissed: (direction) {
-                    showDialog(context: context,
-                        builder: (ctx) => AlertDialog(
-                          title: const Text("Delete Ingredient"),
-                          content: const Text("Are you sure you want to delete this ingredient?"),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  IngredientItem(snapshot.data![index], _selectIngredient);
-                                });
-                                Navigator.of(ctx).pop();
-                              },
-                              child: const Text("No"),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                _deleteIngredient(snapshot.data![index]);
-                                Navigator.of(ctx).pop();
-
-                              },
-                              child: const Text("Yes"),
-                            ),
-                          ],
-                        ),
-                      );
+            child: Column(
+              children: <Widget>[
+                if (_showSearchBar) searchBar,
+                /*Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    onChanged: (value) {
+                      filterSearchResults(value);
                     },
-                    child: IngredientItem(snapshot.data![index], _selectIngredient));
-              },
+                    controller: editingController,
+                    decoration: InputDecoration(
+                        labelText: "Search",
+                        hintText: "Search",
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+                  ),
+                ),*/
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return Dismissible(
+                        key: UniqueKey(),
+                          onDismissed: (direction) {
+                          showDialog(context: context,
+                              builder: (ctx) => AlertDialog(
+                                title: const Text("Delete Ingredient"),
+                                content: const Text("Are you sure you want to delete this ingredient?"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        IngredientItem(items[index], _selectIngredient);
+                                      });
+                                      Navigator.of(ctx).pop();
+                                    },
+                                    child: const Text("No"),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      _deleteIngredient(items[index]);
+                                      Navigator.of(ctx).pop();
+
+                                    },
+                                    child: const Text("Yes"),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                          child: IngredientItem(items[index], _selectIngredient));
+                    },
+                  ),
+                ),
+              ],
             ),
           );
         } else if (snapshot.hasError) {
@@ -141,7 +218,11 @@ class _IngredientsScreenState extends State<IngredientsScreen> {
         actions: [
           IconButton(
             icon: const Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              setState(() {
+                _showSearchBar = !_showSearchBar;
+              });
+            },
           ),
           IconButton(
             icon: const Icon(Icons.add),
