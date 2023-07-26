31fcf6d4966e94593972eca081a824be0bd6405b
yanik23 <ylange@windowslive.com>
2023-07-27 00:26:00 +0200

    finished
    Signed-off-by: yanik23 <ylange@windowslive.com>

--- tb_frontend/lib/screens/menuScreen.dart
+++ tb_frontend/lib/screens/menuScreen.dart
@@ -7,10 +7,8 @@ import 'package:tb_frontend/screens/welcomeScreen.dart';
 import 'clients/clientsScreen.dart';
 import 'dishes/dishesScreen.dart';
 
-
 /// This class is used to display the menu screen of the application.
 class MenuScreen extends StatefulWidget {
-
   /// The username of the logged in user used to display a welcome message.
   final String username;
   const MenuScreen(this.username, {super.key});
@@ -21,12 +19,10 @@ class MenuScreen extends StatefulWidget {
 
 /// This class is used to manage the state of the menu screen.
 class _MenuScreenState extends State<MenuScreen> {
-
   /// This function is used to initialize the state of the menu screen.
   @override
   initState() {
     super.initState();
-
   }
 
   /// This function is used to build the menu screen.
@@ -36,163 +32,195 @@ class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Menu'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: kColorScheme.primary,
-                ),
-                child: Text(
-                  'Welcome ${widget.username} !',
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text('Settings'),
-                onTap: () {
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.info),
-                title: const Text('About'),
-                onTap: () {
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.logout),
-                title: const Text('Logout'),
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (ctx) {
-                      return const WelcomeScreen();
-                    }),
-                  );
-                },
-              ),
-            ],
-          )
-        ),
-        body: GridView(
-          padding: const EdgeInsets.all(16),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
+      appBar: AppBar(
+        title: const Text('Menu'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
           ),
-          children: [
-            TextButton.icon(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (ctx) {
-                      return const DishesScreen('Dishes');
-                    },
-                  ),
-                );
-              },
-              icon: const Icon(Icons.local_dining_sharp, size: 36),
-              label: const Text('Dishes'),
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.grey,
+        ],
+      ),
+
+      /// The drawer contains a logout button that navigates the user back to the welcome screen, a settings button and an about button.
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: kColorScheme.primary,
+            ),
+            child: Text(
+              'Welcome ${widget.username} !',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
               ),
             ),
-            TextButton.icon(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (ctx) {
-                      return const IngredientsScreen();
-                    },
-                  ),
-                );
-              },
-              icon: const Icon(Icons.fastfood, size: 36),
-              label: const Text('Ingredients'),
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.grey,
-              ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text("Settings not implemented yet"),
+                  content: const Text("Implementation will follow soon"),
+                  actions: [
+                    TextButton(
+                      child: const Text("OK"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.info),
+            title: const Text('About'),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text("About The Boka Food App"),
+                  content: const Text(
+                      "This mobile application was developed by Yanik Lange as part of his bachelor thesis in 2023 at the HEIG-VD. The thesis was supervised by Prof. Raileanu and made in cooperation with the company Boka Food SA."),
+                  actions: [
+                    TextButton(
+                      child: const Text("OK"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) {
+                  return const WelcomeScreen();
+                }),
+              );
+            },
+          ),
+        ],
+      )),
+      body: GridView(
+        padding: const EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        children: [
+          TextButton.icon(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) {
+                    return const DishesScreen('Dishes');
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.local_dining_sharp, size: 36),
+            label: const Text('Dishes'),
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.grey,
             ),
-            TextButton.icon(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (ctx) {
-                      return const ClientsScreen();
-                    },
-                  ),
-                );
-              },
-              icon: const Icon(Icons.person, size: 36),
-              label: const Text('Clients'),
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.grey,
-              ),
+          ),
+          TextButton.icon(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) {
+                    return const IngredientsScreen();
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.fastfood, size: 36),
+            label: const Text('Ingredients'),
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.grey,
             ),
-            TextButton.icon(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (ctx) {
-                    return const DeliveriesScreen();
-                  }),
-                );
-              },
-              icon: const Icon(Icons.delivery_dining, size: 36),
-              label: const Text('Deliveries'),
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.grey,
-              ),
+          ),
+          TextButton.icon(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) {
+                    return const ClientsScreen();
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.person, size: 36),
+            label: const Text('Clients'),
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.grey,
             ),
-            TextButton.icon(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (ctx) {
-                    return const StatisticMenuScreen();
-                  }),
-                );
-              },
-              //child: const Text('Stats'),
-              icon: const Icon(Icons.stacked_bar_chart, size: 36),
-              label: const Text('Stats'),
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.grey,
-              ),
+          ),
+          TextButton.icon(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) {
+                  return const DeliveriesScreen();
+                }),
+              );
+            },
+            icon: const Icon(Icons.delivery_dining, size: 36),
+            label: const Text('Deliveries'),
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.grey,
             ),
-            TextButton.icon(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
+          ),
+          TextButton.icon(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) {
+                  return const StatisticMenuScreen();
+                }),
+              );
+            },
+            //child: const Text('Stats'),
+            icon: const Icon(Icons.stacked_bar_chart, size: 36),
+            label: const Text('Stats'),
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.grey,
+            ),
+          ),
+          TextButton.icon(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) {
                   return const QrScannerScreen();
                 }),
-                );
-              },
-              icon: const Icon(Icons.photo_camera_rounded, size: 36),
-              label: const Text('QR Scanner'),
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.grey,
-              ),
+              );
+            },
+            icon: const Icon(Icons.photo_camera_rounded, size: 36),
+            label: const Text('QR Scanner'),
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.grey,
             ),
-          ],
-        ),
-
+          ),
+        ],
+      ),
     );
   }
 }
